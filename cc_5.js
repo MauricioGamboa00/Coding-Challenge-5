4e20a959abb9650418d8399030568b6cf3757034
Mauricio Gamboa <mauriciogamboa00@gmail.com>
2025-02-08 14:47:22 -0500

    Task 2 - Order Details.

--- cc_5.js
+++ cc_5.js
@@ -10,3 +10,18 @@ console.log(`Name: ${customer.name}`); // Logging customer name into console
 console.log(`Age: ${customer.age}`); // Logging customer age into console
 console.log(`Email: ${customer.email}`); // Logging customer email into console
 
+// Task 2 Object Methods
+
+let order = {
+    orderID: 12345,
+    totalAmount: 150,
+    status: "Processing",
+
+    displayOrder: function() {
+        console.log(`Order ID: ${this.orderID}, Amount: $${this.totalAmount}, Status: ${this.status}`);
+    }
+
+}; // Logging of order details in console
+
+order.displayOrder()
+
