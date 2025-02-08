3458e1fb76a6577a5bf7b1772c75225cc1e36872
Mauricio Gamboa <mauriciogamboa00@gmail.com>
2025-02-08 14:39:48 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,12 @@
+// Task 1 Object Properties
+
+let customer =  {
+    name: "John Doe",
+    age: 35,
+    email: "john.doe@email.com"
+}; // Declaring an object with multiple properties
+
+console.log(`Name: ${customer.name}`); // Logging customer name into console
+console.log(`Age: ${customer.age}`); // Logging customer age into console
+console.log(`Email: ${customer.email}`); // Logging customer email into console
+
